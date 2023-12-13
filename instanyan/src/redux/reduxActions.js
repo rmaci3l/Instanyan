e384187593672c27a39daeedb8696448b2c1f522
Richard Maciel <rrichardmaciel@gmail.com>
2023-12-13 18:38:55 -0300

    .

---
+++ instanyan/src/redux/reduxActions.js
@@ -0,0 +1,107 @@
+import axios from 'axios'
+import {createAsyncThunk} from '@reduxjs/toolkit'
+
+const backendURL = 'http://localhost:5000'
+
+export const registerUser = createAsyncThunk(
+    'auth/register',
+    async({name, username, email, password}, {rejectWithValue}) =>{
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }
+            const response = await axios.post(
+                `${backendURL}/auth/register`,{name, username, email, password}, config
+            );
+            return response
+        } catch (error) {
+            if (error.response) {
+                return rejectWithValue(error.response.data.message)
+            }else{
+                return rejectWithValue(error.message)
+            }
+        }
+    }
+)
+
+
+export const userLogin = createAsyncThunk(
+    'auth/login',
+    async({ email, password}, {rejectWithValue}) => {
+        try {
+            const config = {
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+            }
+            const { data } = await axios.post(
+                `${backendURL}/auth/login`, { email, password }, config
+            );
+            localStorage.setItem('userToken', data.userToken)
+            return data
+            
+        } catch (error) {
+            if (error.response){
+                return rejectWithValue(error.response.data.message)
+            }else{
+                return rejectWithValue(error.message)
+            }
+        }
+    }
+)
+
+
+export const updateProfile = createAsyncThunk(
+    'api/user/profile',
+    async(profileData, {getState, rejectWithValue}) => {
+        const token = getState().auth.userToken;
+        try {
+            const config ={
+                headers: {
+                    'Content-Type' : 'application/json',
+                    'authorization' : `Bearer ${token}`
+                },
+            }
+            const response = await axios.post(
+                `${backendURL}/api/user/profile`, profileData, config
+            );
+            console.log(response.data.message)
+            return response            
+            } catch (error) {
+                if (error.response) {
+                    return rejectWithValue(error.response.data.message)
+                }else{
+                    return rejectWithValue(error.message)
+                }
+            }
+        }
+)
+
+
+export const createPost = createAsyncThunk(
+    'api/feed/post',
+    async(userPost, {getState, rejectWithValue}) => {
+        const token = getState().auth.userToken;
+        try {
+            const config ={
+                headers: {
+                    'Content-Type' : 'application/json',
+                    'authorization' : `Bearer ${token}`
+                },
+            }
+            const response = await axios.post(
+                `${backendURL}/api/feed/post`, userPost, config
+            );
+            console.log(response.data.message)
+            return response            
+            } catch (error) {
+                if (error.response) {
+                    return rejectWithValue(error.response.data.message)
+                }else{
+                    return rejectWithValue(error.message)
+                }
+            }
+        }
+)
--- server/nyan-server/models/user.py
+++ server/nyan-server/models/user.py
@@ -17,3 +17,28 @@ class User(UserMixin, Base):
         self.username = username
         self.password = password
         
+class UserProfile(Base):
+    __tablename__ = 'user_profiles'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    profile_image = Column(String)
+    status = Column(String(60))
+    about = Column(String(150))
+    posts = Column(Integer, default=0)
+    followers = Column(Integer, default=0)
+    following = Column(Integer, default=0)
+    user = relationship("User", back_populates="profile")
+    
+    
+class Post(Base):
+    __tablename__ = 'posts'
+    
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', back_populates='posts')    
+    image = Column(String)
+    content = Column(String(1000))
+    hashtags = Column(String(100))
+    likes = Column(Integer, default=0)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
