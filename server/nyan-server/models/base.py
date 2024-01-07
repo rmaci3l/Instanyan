89a6525429466c1a612b20e7e2144405043c508c
Richard Maciel <rrichardmaciel@gmail.com>
2024-01-06 22:28:46 -0300

    .

--- server/nyan-server/models/base.py
+++ server/nyan-server/models/base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgres://richardm:LnEegiR6ll6oY2ixDz39TfOY7ERFHVlx@dpg-cmcutfud3nmc73dfe7b0-a/instanyandb')
+engine = create_engine('postgres://richardm:LnEegiR6ll6oY2ixDz39TfOY7ERFHVlx@dpg-cmcutfud3nmc73dfe7b0-a.oregon-postgres.render.com/instanyandb')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
